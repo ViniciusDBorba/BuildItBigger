1b10491ef8db80020e0ad020b39ea2aae1e00f5c
Vinicius de Borba <vinicius.borba@hbsis.com.br>
2018-11-22 17:56:42 -0200

    Fixes

--- app/src/main/java/com/udacity/gradle/builditbigger/EndpointAsync.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/EndpointAsync.java
@@ -46,7 +46,8 @@ public class EndpointAsync extends AsyncTask<Void, Void, String> {
         try {
             return myApiService.getJoke().execute().getData();
         } catch (IOException e) {
-            return e.getMessage();
+            e.getMessage();
+            return null;
         }
     }
 
